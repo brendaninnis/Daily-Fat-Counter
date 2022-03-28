95abe5a98b8a123d890fd78e6cba4ec348df4952
Brendan Innis <innisbrendan@gmail.com>
2022-03-27 17:18:47 -0700

    Initial Commit

---
+++ Daily Fat Counter/Daily_Fat_CounterApp.swift
@@ -0,0 +1,17 @@
+//
+//  Daily_Fat_CounterApp.swift
+//  Daily Fat Counter
+//
+//  Created by Brendan Innis on 2022-03-27.
+//
+
+import SwiftUI
+
+@main
+struct Daily_Fat_CounterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
